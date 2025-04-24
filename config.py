4dbece8ff6185755fc1b664d26b78e26b3c4f733
WENTY09 <117012644+WENTY09@users.noreply.github.com>
2025-04-25 01:09:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///delivery_bot.db")
+    # Строка подключения к PostgreSQL из Render
+    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://wenty:4tf4gCkox2X0TbrluhFYo522s2BrP44J@dpg-d05b9lruibrs73fi0cm0-a/delivery_bot")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("UvWO2pXY2409JZFIH4GVdKYqgzDpwtn6", "UvWO2pXY2409JZFIH4GVdKYqgzDpwtn6")
