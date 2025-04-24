d933c7fc61b6b75ea3098549ee40b000c61e007a
WENTY09 <117012644+WENTY09@users.noreply.github.com>
2025-04-25 01:23:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     # Строка подключения к PostgreSQL из Render
-    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://wenty:4tf4gCkox2X0TbrluhFYo522s2BrP44J@dpg-d05b9lruibrs73fi0cm0-a/delivery_bot")
+    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://wenty:4tf4gCkox2X0TbrluhFYo522s2BrP44J@dpg-d05b9lruibrs73fi0cm0-a.oregon-postgres.render.com/delivery_bot")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("UvWO2pXY2409JZFIH4GVdKYqgzDpwtn6", "UvWO2pXY2409JZFIH4GVdKYqgzDpwtn6")
